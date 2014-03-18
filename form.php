4f981a4f90c76894ca6aab12ee1c239cd357f615
emealia1 <emealia.hollis@ksuwdc.com>
2014-03-17 21:16:29 -0400

    php

--- form.php
+++ form.php
@@ -3,16 +3,16 @@
 	<head></head>
 	<body>
 		<?php 
-			$fname = $_GET["fname"];
-			$lname = $_GET["lname"];
-			$address = $_GET["address"];
-			$email = $_GET["email"];
-			$zip = $_GET["zip"];
-			$phone = $_GET["phone"];
+			$fname = $_POST["fname"];
+			$lname = $_POST["lname"];
+			$address = $_POST["address"];
+			$email = $_POST["email"];
+			$zip = $_POST["zip"];
+			$phone = $_POST["phone"];
 		?>
 
 		<h1>Hello <?php echo $fname?></h1>
-
+		Here is your info
 		<div>
 		<?php echo $email?>
 		<?php echo $phone?>
